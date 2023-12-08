2f6549f02310be73f1d20847ebb45444940205e8
TheHackerChampion <felix.schweikl@hgtb.de>
2023-12-08 17:18:49 +0100

    idk what this is XD

--- src/routes/navbar.svelte
+++ src/routes/navbar.svelte
@@ -1,112 +1,111 @@
 <script>
-    import "../../static/style.css"
-    import { page } from "$app/stores";
-    import { onMount } from "svelte";
-    import Moon from "./icons/moon.svelte";
-    import Sun from "./icons/sun.svelte";
+  import "../../static/style.css";
+  import { page } from "$app/stores";
+  import { onMount } from "svelte";
+  import Moon from "./icons/moon.svelte";
+  import Sun from "./icons/sun.svelte";
 
-   const navs = [
+  const navs = [
     {
-        title: "Home",
-        href: "/"
+      title: "Home",
+      href: "/",
     },
     {
-        title: "About",
-        href: "/about"
+      title: "About",
+      href: "/about",
     },
     {
-        title: "Contact",
-        href: "/contact"
+      title: "Contact",
+      href: "/contact",
     },
     {
-        title: "Projects",
-        href: "/projects"
+      title: "Projects",
+      href: "/projects",
     },
-   ];
+  ];
 
-   let currentTheme = "";
+  let currentTheme = "";
 
-    onMount(() => {
-        const userPrefDark = window.matchMedia(
-            "(prefers-color-scheme: dark)"
-        ).matches;
+  onMount(() => {
+    const userPrefDark = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
 
-        const hasUserSetDarkMode = 
-            document.documentElement.dataset.theme == "dark";
+    const hasUserSetDarkMode = document.documentElement.dataset.theme == "dark";
 
-        const hasUserSetLightMode =
-        document.documentElement.dataset.theme == "light";
+    const hasUserSetLightMode =
+      document.documentElement.dataset.theme == "light";
 
-        if (!hasUserSetDarkMode && !hasUserSetLightMode) {
-            setTheme(userPrefDark ? "dark" : "light");
-        };
-    });
+    if (!hasUserSetDarkMode && !hasUserSetLightMode) {
+      setTheme(userPrefDark ? "dark" : "light");
+    }
+  });
 
-   // @ts-ignore
-   const setTheme = (theme) => {
-        document.documentElement.dataset.theme = theme;
-        document.cookie = `siteTheme=${theme}; max-age=31536000; path="/"`;
-        currentTheme = theme;
-   };
+  // @ts-ignore
+  const setTheme = (theme) => {
+    document.documentElement.dataset.theme = theme;
+    document.cookie = `siteTheme=${theme}; max-age=31536000; path="/"`;
+    currentTheme = theme;
+  };
 
-    $: routeId = $page.route.id;
+  $: routeId = $page.route.id;
 </script>
 
 <nav>
-    <div class = "container">
-        <h1>Isible</h1>
+  <div class="container">
+    <h1>Isible</h1>
 
-        <ul>
-            {#each navs as {title, href}}
-            <li>
-                <a {href} class:active = {routeId == href} {title}>{title}</a>
-            </li>
-            {/each}
-            <li>
-                {#if currentTheme == "light"}
-                  <a class="moon" href={"#"} on:click={() => setTheme("dark")}>
-                    <Moon />
-                  </a>
-                {:else}
-                  <a class="sun" href={"#"} on:click={() => setTheme("light")}>
-                    <Sun />
-                  </a>
-                {/if}
-              </li>
-        </ul>
-    </div>
+    <ul>
+      {#each navs as { title, href }}
+        <li>
+          <a {href} class:active={routeId == href} {title}>{title}</a>
+        </li>
+      {/each}
+      <li>
+        {#if currentTheme == "light"}
+          <a class="moon" href={"#"} on:click={() => setTheme("dark")}>
+            <Moon />
+          </a>
+        {:else}
+          <a class="sun" href={"#"} on:click={() => setTheme("light")}>
+            <Sun />
+          </a>
+        {/if}
+      </li>
+    </ul>
+  </div>
 </nav>
 
 <style>
-    nav {
-        padding: 0.5em;
-        font-family: lucia;
-        color: white;
-        background-color: rgb(220, 20, 60);
-    }
-    .container {
-        display: flex;
-        align-items: center;
-    }
-    ul {
-        display: flex;
-        margin: 0;
-        margin-left: auto;
-        list-style: none;
-    }
-    li {
-        margin-right: 20px;
-    }
-    h1 {
-        margin: 0;
-        font-size: 1.5em;
-        font-weight: normal;
-    }
-    a {
-        text-decoration: none;
-        color: white;
-    }
-    .active {
-        color: yellow;
-    }
+  nav {
+    padding: 0.5em;
+    font-family: lucia;
+    color: white;
+    background-color: rgb(220, 20, 60);
+  }
+  .container {
+    display: flex;
+    align-items: center;
+  }
+  ul {
+    display: flex;
+    margin: 0;
+    margin-left: auto;
+    list-style: none;
+  }
+  li {
+    margin-right: 20px;
+  }
+  h1 {
+    margin: 0;
+    font-size: 1.5em;
+    font-weight: normal;
+  }
+  a {
+    text-decoration: none;
+    color: white;
+  }
+  .active {
+    color: yellow;
+  }
 </style>
