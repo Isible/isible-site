a6dd90c6c2e99e89c78725d2fd71bccd5cf5a95d
TheHackerChampion <felix.schweikl@hgtb.de>
2023-12-18 19:34:01 +0100

    create about basics

---
+++ src/lib/users.svelte
@@ -0,0 +1,52 @@
+<script>
+    export let name = "";
+
+    let userData;
+  
+    async function fetchData() {
+      try {
+        const response = await fetch('https://api.github.com/users/' + name);
+        const data = await response.json();
+        userData = data;
+      } catch (error) {
+        console.error('Error when getting data:', error);
+      }
+    }
+  
+    fetchData(); // Get data
+  </script>
+  
+  <main>
+    <div class="user">
+      {#if userData}
+      <h1><a href={userData.html_url}>{userData.login}</a></h1>
+      {#if userData.bio}
+        <p>{userData.bio}</p>
+      {/if}
+      <img src={userData.avatar_url} alt="">
+      <!-- Showed data here -->
+    {/if}
+    </div>
+  </main>
+
+
+  <style>
+    img {
+        padding: 1em;
+        border-radius: 20em;
+        height: 15em;
+    }
+
+    a {
+        text-decoration: none;
+        color: var(--color);
+    }
+    a:hover {
+        text-decoration: underline;
+        color: var(--color);
+    }
+
+    .user   {
+        padding-right: 3em;
+    }
+  </style>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,29 @@
-Coming soon...
+<script>
+    import User from "$lib/users.svelte"
+</script>
+
+
+<div class="container">
+    <div class="part">
+        <User name="TheHackerChampion"/>
+        <User name="thepigcat76"/>
+        <User name="TheNumber007"/>
+    </div>
+    <div class="part">
+        <User name="N1cohd1"/>
+        <User name="GS6024"/>
+        <User name="Eli727"/>
+    </div>
+</div>
+
+<style>
+    .container {
+        align-items: center;
+    }
+
+    .part {
+        align-items: center;
+        display: flex;
+        padding-bottom: 1em;
+    }
+</style>
